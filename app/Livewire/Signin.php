4b1644755edb295ddede22050c1c1cc8f16744e0
thekaroe-thailand <thekaroe@gmail.com>
2024-11-27 23:06:27 +0700

    first commit

---
+++ app/Livewire/Signin.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+
+class Signin extends Component
+{
+    public $username;
+    public $password;
+    public $errorUsername;
+    public $errorPassword;
+    public $error = null;
+
+    public function signin() {
+        $this->errorUsername = null;
+        $this->errorPassword = null;
+
+        $validator = Validator::make([
+            'username' => $this->username,
+            'password' => $this->password
+        ], [
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            $this->errorUsername = $validator->errors()->get('username')[0] ?? null;
+            $this->errorPassword = $validator->errors()->get('password')[0] ?? null;
+        } else {
+            $user = User::where('name', $this->username)
+            ->where('password', $this->password)
+            ->first();
+
+            if (!$user) {
+                $this->error = 'ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง';
+            } else {
+                $this->redirect('/dashboard');
+            }
+        }
+    }
+
+    public function render()
+    {
+        return view('livewire.signin');
+    }
+}
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('content')
+    <div>
+        <h3>Dashboard</h3>
+    </div>
+@endsection
